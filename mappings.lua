78b19e3935ba9f5beecfbb602a2d7b772d77c28e
Mateus <mateus.haidar@icloud.com>
2023-10-20 07:54:52 -0300

    first commit

---
+++ mappings.lua
@@ -0,0 +1,15 @@
+local M = {}
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Adiciona breakpoint na linha",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Começa ou continua o debugger",
+    }
+  }
+}
+return M
